2609b396add4986a66dea6c9b2d41f0ee1511d35
Hailey <145402351+haileytrinh@users.noreply.github.com>
2026-08-11 19:08:25 -0500

    Remove Translate instantiation from translate.js
    Removed the instantiation of the Translate object with keyFilename.

--- translate.js
+++ translate.js
@@ -1,10 +1,6 @@
 
 const {Translate} = require('@google-cloud/translate').v2;
 
-const translate = new Translate({
-    keyFilename: './project-3v2-e488b7d3cfbb.json' // Specify the correct path
-});
-
 const translateText = async (text, targetLanguage) => {
     try {
         const [translation] = await translate.translate(text, targetLanguage);
